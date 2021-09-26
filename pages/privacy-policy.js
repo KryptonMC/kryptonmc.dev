ec56467b1f8a8ccee1367b944719f31efd0e68ca
Aidan Bennett <aidanbennett1103@gmail.com>
2021-09-26 14:46:04 +0100

    Added privacy policy page and css and adjusted the tos page

---
+++ pages/privacy-policy.js
@@ -0,0 +1,10 @@
+import styles from '../styles/PrivacyPolicy.module.css';
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>We use cloudflare for analytics and to protect our services</p>
+    </div>
+  );
+}
--- pages/tos.js
+++ pages/tos.js
@@ -1,8 +1,12 @@
+import styles from '../styles.Terms.module.css';
+
 export default function TermsOfService() {
   return (
     <div>
       <h1>
         Insert some generic TOS here and stuff saying we don't sell your data
+        and we may update these terms and conditions at any time without any
+        prior notice.
       </h1>
     </div>
   );

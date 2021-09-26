75f6bfd2a9be3d900204e0affe47043eae2a65a3
Aidan Bennett <aidanbennett1103@gmail.com>
2021-09-26 11:58:40 +0100

    Forgot resources page oops

---
+++ pages/resources.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Resources.module.css';
+
+export default function Resources() {
+  return (
+    <div className={styles.container}>
+      <div className='resources'>
+        <h1>Resources</h1>
+        <p>Coming soon</p>
+      </div>
+    </div>
+  );
+}

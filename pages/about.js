ccc5fc399e51f3df16b316595307f26e23a57585
Aidan Bennett <aidanbennett1103@gmail.com>
2021-09-26 12:05:21 +0100

    Add page titles to all current pages

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,13 @@ import styles from '../styles/About.module.css';
 export default function About() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>kryptonmc.dev | about</title>
+        <meta
+          name='keywords'
+          content='minecraft, mc, minecraft plugins, krypton, krypton plugins, plugins'
+        ></meta>
+      </Head>
       <div className='about'>
         <h1>About</h1>
         <p>
--- pages/resources.js
+++ pages/resources.js
@@ -3,6 +3,13 @@ import styles from '../styles/Resources.module.css';
 export default function Resources() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>kryptonmc.dev | resources coming soon</title>
+        <meta
+          name='keywords'
+          content='minecraft, mc, minecraft plugins, krypton, krypton plugins, plugins'
+        ></meta>
+      </Head>
       <div className='resources'>
         <h1>Resources</h1>
         <p>Coming soon</p>

1864fe1803f84c06ccab2d5a8d19a562c60adbf3
Aidan Bennett <aidanbennett1103@gmail.com>
2021-09-26 11:20:46 +0100

    Change font color and background color

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>
-        A fresh <a href='kryptonmc.org'>Krypton</a> plugin resource site coming
-        soon
-      </h1>
+      <div className='landing'>
+        <h1>
+          A fresh <a href='kryptonmc.org'>Krypton</a> plugin resource site
+          coming soon
+        </h1>
+      </div>
     </div>
   );
 }

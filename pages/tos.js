4b82d068af8a25a872bc201b187d4ae1c9d4f457
Aidan Bennett <aidanbennett1103@gmail.com>
2021-09-26 14:34:46 +0100

    Added TOS page

---
+++ pages/tos.js
@@ -0,0 +1,9 @@
+export default function TermsOfService() {
+  return (
+    <div>
+      <h1>
+        Insert some generic TOS here and stuff saying we don't sell your data
+      </h1>
+    </div>
+  );
+}
